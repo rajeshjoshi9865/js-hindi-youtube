2f70f8d4125c3e89895d3f228411455597768814
rajeshjoshi9865 <111245434+rajeshjoshi9865@users.noreply.github.com>
2025-03-03 16:47:53 +0000

    summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,10 +2,24 @@ let score= "33"
 
 console.log(typeof score)
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 // "33" => 33
 // "33abc"=> NaN
 // true => 1; false =>0
 // "" => false
 // "hitesh"=> true
+
+//*******operations*******/
+let value=3
+let negValue=-value
+//console.log(negValue);
+gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
+
+ 
+
+
+
 
