77a9f821a88f863e7817da046b9beadbb3e2d4ea
rajeshjoshi9865 <111245434+rajeshjoshi9865@users.noreply.github.com>
2025-03-03 10:38:32 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code in newer version
+
+//alert(3+3)// we are using nodejs, not browser
+console.log(3
+    +
+    3) // code readability should be high
+    console.log("Rajesh")
+    let name= "hitesh"
+    let age=18
+    let isLoggedIn=false
+    // number =>2 to power 53
+    // bigint
+    // string =>""
+    // boolean => true/false
+    // null=> standalone value
+    // undefined=>
+    // symbol=> unique
+
+
+    //object
+    console.log(typeof undefined) // undefined
+
+    onsole.log(typeof null) // object
+
+
