fbaf9d51c97e2a72725bcabe3aec32e8140e278b
rajeshjoshi9865 <111245434+rajeshjoshi9865@users.noreply.github.com>
2025-03-03 10:42:42 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="rajeshgoole.com"
+var accountPassword="12345"
+accountCity="jaipur"
+//accountId=2
+accountEmail="jhjah"
+accountPassword="21121"
+accountCity="bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/*
+prefer not to use var
+bacause off issue in block scope and functional scope
+*/
