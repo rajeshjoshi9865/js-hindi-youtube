eb73d517ac35da60f44d84c7557a58d70a99f45e
rajeshjoshi9865 <111245434+rajeshjoshi9865@users.noreply.github.com>
2025-03-03 11:21:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score= "33"
+
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+// "33" => 33
+// "33abc"=> NaN
+// true => 1; false =>0
+// "" => false
+// "hitesh"=> true
+
