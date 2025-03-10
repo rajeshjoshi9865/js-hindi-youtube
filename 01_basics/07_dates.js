d6b40e67db23a97812aadbf6b2f76366d609ea39
rajeshjoshi9865 <111245434+rajeshjoshi9865@users.noreply.github.com>
2025-03-10 11:18:20 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,14 @@
+// Date
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString());
+
+console.log(Date.now());
+
+let newDate = new Date()
+console.log(newDate.getMonth)
+
+
