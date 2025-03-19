bd6ad415e5357b76b1a6138130eaf43792a40cc9
rajeshjoshi9865 <111245434+rajeshjoshi9865@users.noreply.github.com>
2025-03-19 18:30:42 +0000

    object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,35 +1,63 @@
-// const tinderUser = new Object()
-// const tinderUser {}
+// // const tinderUser = new Object()
+// // const tinderUser {}
 
-// tinderUser.id = "123abc"
-// tinderUser.name = "sammy"
-// tinderUser.isLoggedIn = false
+// // tinderUser.id = "123abc"
+// // tinderUser.name = "sammy"
+// // tinderUser.isLoggedIn = false
 
-// //console.log(tinderUSer)
+// // //console.log(tinderUSer)
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
-//const obj2 = {3: "a", 4: "b"}
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// //const obj2 = {3: "a", 4: "b"}
 
-// //const obj3 = {obj1, obj2}
-// const obj3 = Object.assign({}, obj1, obj2)
-// console.log(obj3)
+// // //const obj3 = {obj1, obj2}
+// // const obj3 = Object.assign({}, obj1, obj2)
+// // console.log(obj3)
 
-const obj3 = {obj1, obj2}
-const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+// const obj3 = {obj1, obj2}
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
 
-users = [
-    {
-        {
+// users = [
+//     {
+//         {
 
-        }
-    }
+//         }
+//     }
+// ]
+// users[1].email
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+const course = {
+    courseName: "js in hindi"
+    price: "999"
+    courseInstructor: "hitesh"
+}
+// course.courseInstructor
+const {courseInstructor} = course
+const {courseInstructor: instructor} = course
+console.log(courseInstructor);
+
+{
+    "name": "hitesh"
+    "courseName": "js in hindi",
+    "price": "free"
+}
+[
+    {},
+    {},
+    {}
 ]
-users[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+
+
+
+
 
 
 
